6552bb0a8181e6ccbc6757bbb7e08ee6ca76aa4a
konuleminova <konul.eminova39@gmail.com>
2021-04-28 12:44:33 +0400

    first commit

---
+++ app/src/main/java/com/example/nfcreaderappdemo/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.nfcreaderappdemo
+
+import android.nfc.NfcAdapter
+import android.nfc.Tag
+import android.nfc.tech.IsoDep
+import android.os.Build
+import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+@RequiresApi(Build.VERSION_CODES.KITKAT)
+class MainActivity : AppCompatActivity(), NfcAdapter.ReaderCallback {
+
+    private var nfcAdapter: NfcAdapter? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        nfcAdapter?.enableReaderMode(this, this,
+                NfcAdapter.FLAG_READER_NFC_A or NfcAdapter.FLAG_READER_SKIP_NDEF_CHECK,
+                null)
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        nfcAdapter?.disableReaderMode(this)
+    }
+
+    override fun onTagDiscovered(tag: Tag?) {
+        val isoDep = IsoDep.get(tag)
+        isoDep.connect()
+        val response = isoDep.transceive(Utils.hexStringToByteArray(
+                "00A4040007A0000002471001"))
+        runOnUiThread { textView.append("\nCard Response: "
+                +Utils.toHex(response)) }
+        isoDep.close()
+    }
+
+}
